aa6278990abe3fef86f8973374bb5951e92de689
PholSoophak <sokphakphol@gmail.com>
2024-09-13 09:15:29 +0700

    Automate Test with Jenkins

---
+++ Test Suites/Jenkins_TestSuit.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Jenkins_TestSuit</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>fad1af98-3eb1-4f4f-99f7-e5ac1bf70d3b</testSuiteGuid>
+</TestSuiteEntity>
