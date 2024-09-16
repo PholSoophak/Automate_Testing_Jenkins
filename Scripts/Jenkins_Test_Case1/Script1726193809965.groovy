e7aba8eaf67617aafabbe0da1a9e3792126ab451
PholSoophak <sokphakphol@gmail.com>
2024-09-16 15:36:40 +0700

    upadate new script Test with jenkins

--- Scripts/Jenkins_Test_Case1/Script1726193809965.groovy
+++ Scripts/Jenkins_Test_Case1/Script1726193809965.groovy
@@ -17,3 +17,13 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.kbprasacbank.com.kh/')
+
+WebUI.delay(5)
+
+WebUI.verifyTextPresent('merl ERROR  !!!', false, FailureHandling.CONTINUE_ON_FAILURE)
+
+WebUI.closeBrowser()
+
--- Test Suites/Jenkins_TestSuit.ts
+++ Test Suites/Jenkins_TestSuit.ts
@@ -5,10 +5,24 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>fad1af98-3eb1-4f4f-99f7-e5ac1bf70d3b</testSuiteGuid>
+   <testCaseLink>
+      <guid>978c0f99-02f9-403b-ad56-02f56767aa94</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Jenkins_Test_Case1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0b38a744-8885-4ce0-b254-9fd324c0c839</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Jenkins_Test_Case2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
